19d7fbb1e6e59835fd3f9d7d5c4a70ba85d8a9e7
Mike Adeleke <madeleke13@gmail.com>
2013-10-29 13:50:26 -0500

    Uodated the show and index pages and changed the root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SampleApp::Application.routes.draw do
   resources :properties
 
   devise_for :users
-  root to: 'static_pages#home'
+  root to: 'properties#index'
   match '/help',    to: 'static_pages#help', via: 'get'
   match '/about',   to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
